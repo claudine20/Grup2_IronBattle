88dc22b778aac3767d16344e25dcf353940d7879
grace <kellygrs@gmail.com>
2025-07-22 01:05:49 +0200

    Constructor extension, attack description, vitals description

--- src/Warrior.java
+++ src/Warrior.java
@@ -8,8 +8,8 @@ public class Warrior extends Character implements Attacker  {
     private int strength;
 
 //Constructors
-    public Warrior(char value, String name, int hp, int stamina, int strength) {
-        super(value);
+    public Warrior( String name, int hp, int stamina, int strength) {
+        super(name,hp);
         this.name = name;
         this.hp = hp;
         this.Stamina = stamina;
@@ -31,10 +31,8 @@ public class Warrior extends Character implements Attacker  {
     public void setStrength(int strength) {
         this.strength = strength;
     }
-    @Override
-    public void attack(){
 
-    }
+
 
     public int getHp() {
         return hp;
@@ -56,27 +54,34 @@ public class Warrior extends Character implements Attacker  {
     public void attack(Character opponent){
 //        "What kind of attack you want to make? 1-Heavy attack, 2-Weak attack";
         int typeOfAttack= ((int) (Math.random()*2)+1);
-        int newHp;
-        System.out.println(typeOfAttack);
+        int newHp = 0;
+//        System.out.println(typeOfAttack);
         if (typeOfAttack==1&&Stamina>10){
 //Heavy attack
+            System.out.println(this.getName()+ " makes a heavy attack on "+opponent.getName());
             Stamina=getStamina()-5;
             newHp = opponent.getHp() - getStrength();
-            System.out.println(newHp);
-            System.out.println(Stamina);
+          
 
         }else if(Stamina<10){
-
+            System.out.println(this.getName()+ " does not have the stamina to make an attack he will not inflict any damage and recover his stamina");
             Stamina=getStamina()+2;
-            System.out.println(Stamina);
-        }else {
+           
+        }
+//        else if (this.hp==0) {
+//            System.out.println(this.getName()+ " is not alive");
+//
+//        }
+        else {
             //Weak attack
+            System.out.println(this.getName()+ " makes a weak attack on "+opponent.getName());
             Stamina=getStamina()+1;
             newHp = opponent.getHp()- (getStrength()/2);
-            System.out.println(Stamina);
-            System.out.println(newHp);
+           
         }
-
+        System.out.println(this.getName()+" stamina: "+Stamina);
+        System.out.println(opponent.getName()+" hp: "+newHp);
+        System.out.println(this.getName()+" strength: "+ strength);
 
 
     }
